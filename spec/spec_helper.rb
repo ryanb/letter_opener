db4660760b66f7ff378cd0da781ce5fa44b4404b
Ryan Bates <ryan@railscasts.com>
2011-09-08 16:11:48 -0700

    reverting to position: absolute because I was getting a large gap in Safari

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,4 +6,7 @@ require "mail"
 require "launchy"
 
 RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
 end
