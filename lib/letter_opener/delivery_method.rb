eb765416d491ea1bc8789b691282e3ee9d396e0f
David Cornu <davidjcornu@gmail.com>
2012-05-10 18:31:51 -0400

    Use mail's text_part and html_part methods to avoid rendering attachments as plain text

--- lib/letter_opener/delivery_method.rb
+++ lib/letter_opener/delivery_method.rb
@@ -6,7 +6,9 @@ module LetterOpener
 
     def deliver!(mail)
       location = File.join(@options[:location], "#{Time.now.to_i}_#{Digest::SHA1.hexdigest(mail.encoded)[0..6]}")
-      messages = mail.parts.map { |part| Message.new(location, mail, part) }
+      messages = []
+      messages << Message.new(location, mail, mail.html_part) if mail.html_part
+      messages << Message.new(location, mail, mail.text_part) if mail.text_part
       messages << Message.new(location, mail) if messages.empty?
       messages.each { |message| message.render }
       Launchy.open(URI.parse("file://#{messages.first.filepath}"))
