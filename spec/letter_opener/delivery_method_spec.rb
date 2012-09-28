a9d573859e7b349d18e3b76b5f6706197e98886b
Vasiliy Ermolovich <younash@gmail.com>
2012-09-28 10:57:03 +0300

    refactor delivery method specs

--- spec/letter_opener/delivery_method_spec.rb
+++ spec/letter_opener/delivery_method_spec.rb
@@ -13,78 +13,148 @@ describe LetterOpener::DeliveryMethod do
     end
   end
 
-  it "saves text into html document" do
-    Launchy.should_receive(:open)
-    mail = Mail.deliver do
-      from     'Foo foo@example.com'
-      reply_to 'No Reply no-reply@example.com'
-      to       'bar@example.com'
-      subject  'Hello'
-      body     'World!'
-    end
-    text = File.read(Dir["#{location}/*/plain.html"].first)
-    text.should include("Foo foo@example.com")
-    text.should include("No Reply no-reply@example.com")
-    text.should include("bar@example.com")
-    text.should include("Hello")
-    text.should include("World!")
-  end
+  context 'content' do
+    let(:plain_file) { Dir["#{location}/*/plain.html"].first }
+    let(:plain) { File.read(plain_file) }
 
-  it "saves multipart email into html document" do
-    mail = Mail.deliver do
-      from    'foo@example.com'
-      to      'bar@example.com'
-      subject 'Many parts'
-      text_part do
-        body 'This is <plain> text'
+    context 'plain' do
+      before do
+        Launchy.should_receive(:open)
+
+        Mail.deliver do
+          from     'Foo foo@example.com'
+          reply_to 'No Reply no-reply@example.com'
+          to       'bar@example.com'
+          subject  'Hello'
+          body     'World!'
+        end
       end
-      html_part do
-        content_type 'text/html; charset=UTF-8'
-        body '<h1>This is HTML</h1>'
+
+      it 'creates plain html document' do
+        File.exist?(plain_file).should be_true
+      end
+
+      it 'saves From field' do
+        plain.should include("Foo foo@example.com")
+      end
+
+      it 'saves Reply-to field' do
+        plain.should include("No Reply no-reply@example.com")
+      end
+
+      it 'saves To field' do
+        plain.should include("bar@example.com")
+      end
+
+      it 'saves Subject field' do
+        plain.should include("Hello")
+      end
+
+      it 'saves Body field' do
+        plain.should include("World!")
       end
     end
-    text = File.read(Dir["#{location}/*/plain.html"].first)
-    text.should include("View HTML version")
-    text.should include("This is &lt;plain&gt; text")
-    html = File.read(Dir["#{location}/*/rich.html"].first)
-    html.should include("View plain text version")
-    html.should include("<h1>This is HTML</h1>")
+
+    context 'multipart' do
+      let(:rich_file) { Dir["#{location}/*/rich.html"].first }
+      let(:rich) { File.read(rich_file) }
+
+      before do
+        Launchy.should_receive(:open)
+
+        Mail.deliver do
+          from    'foo@example.com'
+          to      'bar@example.com'
+          subject 'Many parts'
+          text_part do
+            body 'This is <plain> text'
+          end
+          html_part do
+            content_type 'text/html; charset=UTF-8'
+            body '<h1>This is HTML</h1>'
+          end
+        end
+      end
+
+      it 'creates plain html document' do
+        File.exist?(plain_file).should be_true
+      end
+
+      it 'creates rich html document' do
+        File.exist?(rich_file).should be_true
+      end
+
+      it 'shows link to rich html version' do
+        plain.should include("View HTML version")
+      end
+
+      it 'saves text part' do
+        plain.should include("This is &lt;plain&gt; text")
+      end
+
+      it 'saves html part' do
+        rich.should include("<h1>This is HTML</h1>")
+      end
+    end
   end
 
-  it "saves text into html document with spaces in name" do
-    location = File.expand_path('../../../tmp/letter_opener with space', __FILE__)
+  context 'document with spaces in name' do
+    let(:location) { File.expand_path('../../../tmp/letter_opener with space', __FILE__) }
+    let(:file)     { Dir["#{location}/*/plain.html"].first }
+    let(:plain)    { File.read(file) }
 
-    Mail.defaults do
-      delivery_method LetterOpener::DeliveryMethod, :location => location
+    before do
+      Launchy.should_receive(:open)
+
+      Mail.deliver do
+        from     'Foo foo@example.com'
+        to       'bar@example.com'
+        subject  'Hello'
+        body     'World!'
+      end
     end
 
-    Launchy.should_receive(:open)
-    mail = Mail.deliver do
-      from     'Foo foo@example.com'
-      to       'bar@example.com'
-      subject  'Hello'
-      body     'World!'
+    it 'creates plain html document' do
+      File.exist?(file)
     end
-    text = File.read(Dir["#{location}/*/plain.html"].first)
-    text.should include("Foo foo@example.com")
+
+    it 'saves From filed' do
+      plain.should include("Foo foo@example.com")
+    end
   end
 
-  it "saves text into html document when deliver! is called" do
-    Launchy.should_receive(:open)
-    mail = Mail.new do
-      from    'foo@example.com'
-      to      'bar@example.com'
-      subject 'Hello'
-      body    'World!'
+  context 'using deliver! method' do
+    let(:plain_file) { Dir["#{location}/*/plain.html"].first }
+    let(:plain) { File.read(plain_file) }
+
+    before do
+      Launchy.should_receive(:open)
+      Mail.new do
+        from    'foo@example.com'
+        to      'bar@example.com'
+        subject 'Hello'
+        body    'World!'
+      end.deliver!
     end
 
-    mail.deliver!
+    it 'creates plain html document' do
+      File.exist?(plain_file).should be_true
+    end
 
-    text = File.read(Dir["#{location}/*/plain.html"].first)
-    text.should include("foo@example.com")
-    text.should include("bar@example.com")
-    text.should include("Hello")
-    text.should include("World!")
+    it 'saves From field' do
+      plain.should include("foo@example.com")
+    end
+
+    it 'saves To field' do
+      plain.should include("bar@example.com")
+    end
+
+    it 'saves Subject field' do
+      plain.should include("Hello")
+    end
+
+    it 'saves Body field' do
+      plain.should include("World!")
+    end
   end
 end
-
