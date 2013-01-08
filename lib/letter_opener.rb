da9c6aa8675c10f6a9069998d122bac481d13ba1
Agustin Cavilliotti <cavi21[at]gmail.com>
2013-01-08 20:04:31 -0300

    Some fixes

--- lib/letter_opener.rb
+++ lib/letter_opener.rb
@@ -7,7 +7,7 @@ require "launchy"
 require "letter_opener/message"
 require "letter_opener/delivery_method"
 if defined? Rails
-	require "letter_opener/railtie" 
-	major, minor = Rails.version.split('.')
-	require "actionmailer/letter_opener_extension" if major == '2' && minor == '3'
+  require "letter_opener/railtie" 
+  major, minor = Rails.version.split('.')
+  require "actionmailer/letter_opener_extension" if major == '2' && minor == '3'
 end
