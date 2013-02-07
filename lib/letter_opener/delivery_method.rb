036190a0bcaef9e0f203055ff9eae013d0f09369
Bram Plessers <webdevotion>
2013-02-07 22:23:37 +0100

    removed the 'file' prefix and URI.parse call

--- lib/letter_opener/delivery_method.rb
+++ lib/letter_opener/delivery_method.rb
@@ -12,7 +12,7 @@ module LetterOpener
     def deliver!(mail)
       location = File.join(settings[:location], "#{Time.now.to_i}_#{Digest::SHA1.hexdigest(mail.encoded)[0..6]}")
       messages = Message.rendered_messages(location, mail)
-      Launchy.open("file://" + URI.parse(URI.escape(messages.first.filepath)).to_s)
+      Launchy.open(URI.escape(messages.first.filepath))
     end
   end
 end
