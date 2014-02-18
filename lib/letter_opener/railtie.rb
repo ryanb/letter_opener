5e089ba2c600f8a902c49b8ae157b845c504a96a
Akira Matsuda <ronnie@dio.jp>
2014-02-18 19:21:29 +0900

    Lazy-load ActionMailer::Base to defer :action_mailer on_load hook

--- lib/letter_opener/railtie.rb
+++ lib/letter_opener/railtie.rb
@@ -1,7 +1,9 @@
 module LetterOpener
   class Railtie < Rails::Railtie
     initializer "letter_opener.add_delivery_method" do
-      ActionMailer::Base.add_delivery_method :letter_opener, LetterOpener::DeliveryMethod, :location => Rails.root.join("tmp", "letter_opener")
+      ActiveSupport.on_load :action_mailer do
+        ActionMailer::Base.add_delivery_method :letter_opener, LetterOpener::DeliveryMethod, :location => Rails.root.join("tmp", "letter_opener")
+      end
     end
   end
 end
