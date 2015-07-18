189555bc778ed2cfa433cc5361e6ad0d19dda785
Florian Weingarten <flo@hackvalue.de>
2015-07-18 02:39:42 +0000

    Fix non-Rails with actionmailer 4.2.3

--- lib/letter_opener.rb
+++ lib/letter_opener.rb
@@ -6,4 +6,4 @@ require "launchy"
 
 require "letter_opener/message"
 require "letter_opener/delivery_method"
-require "letter_opener/railtie" if defined? Rails
+require "letter_opener/railtie" if defined?(Rails) && defined?(Rails::Railtie)
