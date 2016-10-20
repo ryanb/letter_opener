fdf657d07df32bced21e1a2be3b62c19a3f88cdb
Piotr Usewicz <piotr@layer22.com>
2016-10-20 13:40:12 +0200

    Increase timestamp precision
    This helps with selecting emails sent in the same second while selecting the most recent email programmatically.

--- lib/letter_opener/delivery_method.rb
+++ lib/letter_opener/delivery_method.rb
@@ -22,7 +22,7 @@ module LetterOpener
     def deliver!(mail)
       check_delivery_params(mail) if respond_to?(:check_delivery_params)
 
-      location = File.join(settings[:location], "#{Time.now.to_i}_#{Digest::SHA1.hexdigest(mail.encoded)[0..6]}")
+      location = File.join(settings[:location], "#{Time.now.to_f.to_s.tr('.', '_')}_#{Digest::SHA1.hexdigest(mail.encoded)[0..6]}")
       messages = Message.rendered_messages(location, mail)
       Launchy.open("file:///#{URI.parse(URI.escape(messages.first.filepath))}")
     end
