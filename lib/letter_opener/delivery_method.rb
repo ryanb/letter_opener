f7853314ce771b01c7a315bf3f3811e291d3005d
Ben Hull <ben@spookandpuff.com>
2017-03-31 17:26:06 +1030

    Allow messages to be rendered without headers, container, metadata, etc.

--- lib/letter_opener/delivery_method.rb
+++ lib/letter_opener/delivery_method.rb
@@ -13,17 +13,22 @@ module LetterOpener
     class InvalidOption < StandardError; end
 
     attr_accessor :settings
+    
+    DEFAULTS = {
+      template: 'default'
+    }.freeze
 
     def initialize(options = {})
       raise InvalidOption, "A location option is required when using the Letter Opener delivery method" if options[:location].nil?
-      self.settings = options
+      self.settings = DEFAULTS.merge(options)
     end
 
     def deliver!(mail)
       check_delivery_params(mail) if respond_to?(:check_delivery_params)
 
       location = File.join(settings[:location], "#{Time.now.to_f.to_s.tr('.', '_')}_#{Digest::SHA1.hexdigest(mail.encoded)[0..6]}")
-      messages = Message.rendered_messages(location, mail)
+      messages = Message.rendered_messages(location, mail, settings)
+
       Launchy.open("file:///#{URI.parse(URI.escape(messages.first.filepath))}")
     end
   end
