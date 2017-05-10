fda189ae77fb7208b06b84002625ce7ba615e903
Ben Hull <ben@spookandpuff.com>
2017-05-10 22:52:06 +0930

    Use the string 'default' for the default template name

--- lib/letter_opener/railtie.rb
+++ lib/letter_opener/railtie.rb
@@ -3,7 +3,7 @@ module LetterOpener
     initializer "letter_opener.add_delivery_method" do
       ActiveSupport.on_load :action_mailer do
         location = ENV['LETTER_OPENER_LOCATION'] || Rails.root.join("tmp", "letter_opener")
-        template = ENV['LETTER_OPENER_MESSAGE_TEMPLATE'] || :default
+        template = ENV['LETTER_OPENER_MESSAGE_TEMPLATE'] || 'default'
         ActionMailer::Base.add_delivery_method :letter_opener, LetterOpener::DeliveryMethod, {location: location, template: template}
       end
     end
