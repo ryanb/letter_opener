8c5206b8d31309ea95be15a912bc4e70d8ee7c43
Vasiliy Ermolovich <younash@gmail.com>
2018-01-03 14:42:02 +0300

    Do not parse and escape url before pass it to Launchy.
    Launchy uses Addressable gem on passed url so I guess it should handle it.
    Fixes #148

--- lib/letter_opener/delivery_method.rb
+++ lib/letter_opener/delivery_method.rb
@@ -21,7 +21,7 @@ module LetterOpener
       location = File.join(settings[:location], "#{Time.now.to_f.to_s.tr('.', '_')}_#{Digest::SHA1.hexdigest(mail.encoded)[0..6]}")
 
       messages = Message.rendered_messages(mail, location: location, message_template: settings[:message_template])
-      Launchy.open("file:///#{URI.parse(CGI.escape(messages.first.filepath))}")
+      Launchy.open("file:///#{messages.first.filepath}")
     end
 
     private
