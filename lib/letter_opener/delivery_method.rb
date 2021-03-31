5f5ebd68d70020fbd891db1a38d895cdf10d1981
Kulgar <regmillet@ct2c.fr>
2021-03-31 19:24:20 +0200

    Allow configuration of the 'file///' part in Launchy.open path

--- lib/letter_opener/delivery_method.rb
+++ lib/letter_opener/delivery_method.rb
@@ -10,6 +10,7 @@ module LetterOpener
     def initialize(options = {})
       options[:message_template] ||= LetterOpener.configuration.message_template
       options[:location] ||= LetterOpener.configuration.location
+      options[:files_storage] ||= LetterOpener.configuration.files_storage
 
       raise InvalidOption, "A location option is required when using the Letter Opener delivery method" if options[:location].nil?
 
@@ -21,7 +22,7 @@ module LetterOpener
       location = File.join(settings[:location], "#{Time.now.to_f.to_s.tr('.', '_')}_#{Digest::SHA1.hexdigest(mail.encoded)[0..6]}")
 
       messages = Message.rendered_messages(mail, location: location, message_template: settings[:message_template])
-      Launchy.open("file:///#{messages.first.filepath}")
+      Launchy.open("#{settings[:files_storage]}#{messages.first.filepath}")
     end
 
     private
