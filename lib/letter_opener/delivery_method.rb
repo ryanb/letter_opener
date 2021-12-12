c67cc5b444b3b4ff394f81ee5b9756d0a766f311
Vasiliy Ermolovich <younash@gmail.com>
2021-12-12 19:05:22 +0300

    Make sure that global Launchy constant is used.

--- lib/letter_opener/delivery_method.rb
+++ lib/letter_opener/delivery_method.rb
@@ -22,7 +22,7 @@ module LetterOpener
       location = File.join(settings[:location], "#{Time.now.to_f.to_s.tr('.', '_')}_#{Digest::SHA1.hexdigest(mail.encoded)[0..6]}")
 
       messages = Message.rendered_messages(mail, location: location, message_template: settings[:message_template])
-      Launchy.open("#{settings[:file_uri_scheme]}#{messages.first.filepath}")
+      ::Launchy.open("#{settings[:file_uri_scheme]}#{messages.first.filepath}")
     end
 
     private
